2f11dd0493d2682c42e689144ed497d1fe391ba2
Wolkenbauer Internet KG <36984364+wolkenbauer@users.noreply.github.com>
2020-02-25 19:20:21 +0100

    Add files via upload

---
+++ cancelinvoice_hook.php
@@ -0,0 +1,31 @@
+<?php
+
+if (!defined("WHMCS"))
+    die("This file cannot be accessed directly");
+
+use WHMCS\Database\Capsule;
+
+add_hook('AfterCronJob',1,function($vars) 
+{
+//
+// Change $overduedays to whatever you need. Default is cancel all invoices after being overdue for 90 days.
+//
+$overduedays=14;    
+$invoices=Capsule::table("tblinvoices")->where("status","Unpaid")->where("duedate","<",date("Y-m-d",strtotime("-$overduedays days")))->get();
+
+foreach($invoices as $invoice)
+{
+  //use UpdateInvoice API Call for changing the status, otherwise the InvoiceCancelled hook will not trigger 
+  //and may affect other modules/apps which rely on this hook.
+  $results = localAPI("UpdateInvoice",array("invoiceid"=>$invoice->id,"status"=>"Cancelled"));
+  if ($results["result"]=="success") 
+   logactivity("[INVOICECANCELHOOK] has cancelled Invoice ID: $invoice->id automatically." ]);
+}
+
+}); //hook
+
+
+add_hook('InvoiceCancelled',1,function($vars) 
+{
+logactivity("[INVOICECANCELHOOK] ".$vars['id']." has been cancelled.";
+});
